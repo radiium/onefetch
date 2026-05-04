83f666cf925c28c238b154a31811f4d92466abd2
radiium <remip.dev@gmail.com>
2026-05-04 23:54:02 +0200

    feat(frontend): rework select directory modal

--- frontend/src/lib/components/SelectDirectory.svelte
+++ frontend/src/lib/components/SelectDirectory.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
-	import { Button, Flex, Floating, Text } from 'svxui';
+	import { XIcon } from 'phosphor-svelte';
+	import { type Snippet } from 'svelte';
+	import { Button, Dialog, Flex, Input, Panel, Separator, Text } from 'svxui';
 
 	type Props = {
 		options: string[];
@@ -10,46 +11,66 @@
 	};
 	let { options = [], disabled, onSelect, children }: Props = $props();
 
-	let isOpen = $state(false);
+	let isOpen: boolean = $state(false);
+	let query: string = $state('');
+	let inputEl: HTMLInputElement | undefined = $state();
+	let filteredOptions: string[] = $derived(
+		query.trim()
+			? options.filter((opt) => opt.toLowerCase().includes(query.trim().toLowerCase()))
+			: options
+	);
+	const open = async () => {
+		isOpen = true;
+		query = '';
+		setTimeout(() => inputEl?.focus(), 800);
+	};
+	const close = () => {
+		isOpen = false;
+		query = '';
+	};
 </script>
 
-<Floating
-	size="2"
-	variant="soft"
-	outline
-	placement="bottom-end"
-	autoUpdate
-	offset={4}
-	flip
-	closeOnOutsideClick
-	closeOnScroll
-	closeOnEscape
-	closeOnResize
-	bind:isOpen
->
-	{#snippet trigger()}
-		<Button size="3" iconOnly variant="outline" onclick={() => (isOpen = !isOpen)} {disabled}>
-			{@render children?.()}
-		</Button>
-	{/snippet}
-	{#snippet content()}
-		<Flex direction="column" gap="1" align="center" style="min-width: 250px">
-			{#each options as opt, i (i)}
-				<Button
-					variant="clear"
-					align="start"
+<Button size="3" iconOnly variant="outline" onclick={open} {disabled}>
+	{@render children?.()}
+</Button>
+
+<Dialog closeOnBackdropClick closeOnEscape bind:isOpen onClose={close} style="margin-top: 10%">
+	<Panel variant="soft" outline>
+		<Flex direction="column" gap="3" width="350px" maxHeight="70vh">
+			<Flex align="center" gap="3">
+				<Input
+					id="search-directory"
+					type="text"
+					placeholder="Search directory..."
 					fullWidth
-					onclick={() => {
-						isOpen = false;
-						onSelect?.(opt);
-					}}
-					style="--button-background-hover: var(--accent-5);"
-				>
-					<Text wrap="nowrap">
-						{opt}
-					</Text>
+					bind:value={query}
+					bind:ref={inputEl}
+				/>
+				<Button iconOnly onclick={close}>
+					<XIcon />
 				</Button>
-			{/each}
+			</Flex>
+
+			<Separator size="4" />
+			<Flex direction="column" gap="1" overflow="auto">
+				{#if filteredOptions.length === 0}
+					<Text wrap="nowrap">No result found...</Text>
+				{/if}
+				{#each filteredOptions as opt, i (i)}
+					<Button
+						variant="clear"
+						align="start"
+						fullWidth
+						onclick={() => {
+							close();
+							onSelect?.(opt);
+						}}
+						style="--button-background-hover: var(--accent-5);"
+					>
+						<Text wrap="nowrap">{opt}</Text>
+					</Button>
+				{/each}
+			</Flex>
 		</Flex>
-	{/snippet}
-</Floating>
+	</Panel>
+</Dialog>
